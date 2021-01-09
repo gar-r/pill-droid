76cb14fa3ae56fbb1367abf2a96b298104d4ddc8
Richard Garai <garric@okki.hu>
2021-01-09 17:54:07 +0100

    about menu item

--- app/src/main/java/hu/okki/pilldroid/MainActivity.kt
+++ app/src/main/java/hu/okki/pilldroid/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
@@ -21,17 +22,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_about -> {
+                val navController = findNavController(R.id.nav_host_fragment)
+                navController.navigate(R.id.aboutFragment)
+                return true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
