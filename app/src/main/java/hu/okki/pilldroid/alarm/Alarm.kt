00af732b08da10980a73a7ebf744731b35552185
Richard Garai <garric@okki.hu>
2021-03-12 21:09:11 +0100

    alarm manager improved

--- app/src/main/java/hu/okki/pilldroid/alarm/Alarm.kt
+++ app/src/main/java/hu/okki/pilldroid/alarm/Alarm.kt
@@ -1,10 +1,10 @@
 package hu.okki.pilldroid.alarm
 
 import android.app.AlarmManager
-import android.app.AlarmManager.INTERVAL_DAY
 import android.app.AlarmManager.RTC_WAKEUP
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_NO_CREATE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Context.ALARM_SERVICE
 import android.content.Intent
@@ -13,33 +13,15 @@ import android.util.Log
 import hu.okki.pilldroid.data.medList
 import hu.okki.pilldroid.data.medListOld
 import hu.okki.pilldroid.model.Dosage
-import hu.okki.pilldroid.model.Medication
-import java.lang.Integer.parseInt
+import kotlinx.coroutines.awaitAll
 import java.util.*
-import java.util.concurrent.ThreadPoolExecutor
 
 fun updateAlarms(context: Context) {
     val alarmManager = context.getSystemService(ALARM_SERVICE) as AlarmManager
-
-    val dosages = medList.flatMap { med -> med.dosages }
-    val oldDosages = medListOld.flatMap { med -> med.dosages }
-
-    dosages.forEach { current ->
-        val old = findById(oldDosages, current)
-        if (old == null) {
-            setAlarm(alarmManager, context, current)
-        } else if (needsUpdate(current, old)) {
-            cancelAlarm(alarmManager, context, old)
-            setAlarm(alarmManager, context, current)
-        }
-    }
-
-    oldDosages.forEach { old ->
-        val current = findById(dosages, old)
-        if (current == null) {
-            cancelAlarm(alarmManager, context, old)
-        }
-    }
+    val new = medList.flatMap { it.dosages }
+    val old = medListOld.flatMap { it.dosages }
+    old.filter { findById(new, it) == null }.forEach { cancelAlarm(alarmManager, context, it) }
+    new.forEach { setAlarm(alarmManager, context, it) }
 }
 
 fun reschedule(context: Context, dosage: Dosage) {
@@ -47,17 +29,9 @@ fun reschedule(context: Context, dosage: Dosage) {
     setAlarm(alarmManager, context, dosage)
 }
 
-private fun findById(list: List<Dosage>, dosage: Dosage): Dosage? {
-    return list.firstOrNull { d -> dosage.id == d.id }
-}
-
-private fun needsUpdate(d1: Dosage, d2: Dosage): Boolean {
-    return d1.hour != d2.hour || d1.minute != d2.minute
-}
-
 private fun setAlarm(alarmManager: AlarmManager, context: Context, dosage: Dosage) {
     val intent = getIntent(context, dosage)
-    val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+    val pendingIntent = PendingIntent.getBroadcast(context, dosage.id, intent, 0)
     alarmManager.setExactAndAllowWhileIdle(
         RTC_WAKEUP,
         getAlarmTime(dosage),
@@ -67,10 +41,16 @@ private fun setAlarm(alarmManager: AlarmManager, context: Context, dosage: Dosag
 
 private fun cancelAlarm(alarmManager: AlarmManager, context: Context, dosage: Dosage) {
     val intent = getIntent(context, dosage)
-    val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, FLAG_NO_CREATE)
+    val pendingIntent = PendingIntent.getBroadcast(context, dosage.id, intent, FLAG_NO_CREATE)
     if (pendingIntent != null) {
         alarmManager.cancel(pendingIntent)
     }
+}
+
+private fun getIntent(context: Context, dosage: Dosage): Intent {
+    val intent = Intent(context, AlarmReceiver::class.java)
+    intent.data = Uri.parse("pilldroid://dosages/${dosage.id}")
+    return intent
 }
 
 private fun getAlarmTime(dosage: Dosage): Long {
@@ -79,7 +59,7 @@ private fun getAlarmTime(dosage: Dosage): Long {
         set(Calendar.MINUTE, dosage.minute)
         set(Calendar.SECOND, 0)
         if (isElapsed(this)) {
-            add(Calendar.DAY_OF_MONTH, 1)
+            add(Calendar.DAY_OF_MONTH, Integer.parseInt(dosage.frequency))
         }
         return timeInMillis
     }
@@ -91,9 +71,7 @@ private fun isElapsed(calendar: Calendar): Boolean {
     }
 }
 
-private fun getIntent(context: Context, dosage: Dosage): Intent {
-    val intent = Intent(context, AlarmReceiver::class.java)
-    intent.data = Uri.parse("pilldroid://dosages/${dosage.id}")
-    return intent
+private fun findById(list: List<Dosage>, dosage: Dosage): Dosage? {
+    return list.firstOrNull { d -> dosage.id == d.id }
 }
 
